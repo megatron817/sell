1814f525dcf1ce84bd79629942dc4e3251713b3e
liutw <liutw@ezgoal.cn>
2019-12-26 17:57:25 +0800

    熟悉vant的DatetimePicker组件

--- src/views/seller/index.vue
+++ src/views/seller/index.vue
@@ -1,37 +1,24 @@
 <template>
   <div class="seller">
-    <!-- 邮政编码 -->
-    <van-divider>输入邮政编码</van-divider>
-    <van-field
-      readonly
-      clickable
-      placeholder="点此输入邮政编码"
-      style="margin-bottom:20px;"
-      :value="value"
-      @touchstart.native.stop="show = true"
-    />
-    <van-number-keyboard
-      v-model="value"
-      title="数字键盘"
-      :show="show"
-      :maxlength="6"
-      @blur="show = false"
-    />
-    <!-- 密码 -->
-    <van-divider>输入密码</van-divider>
-    <van-password-input
-      :value="passwordVal"
-      info="密码为6位数字"
-      :focused="showKeyboard"
-      @focus="showKeyboard = true"
-    />
-    <van-number-keyboard
-      :show="showKeyboard"
-      title="密码键盘"
-      @input="onInput"
-      @delete="onDelete"
-      @blur="showKeyboard = false"
-    />
+    <van-cell
+      title="开始时间"
+      is-link
+      :value-class="className"
+      :value="timeValue"
+      @click="showPopup" />
+    <van-popup v-model="show" position="bottom">
+      <van-datetime-picker
+        v-model="currentDate"
+        type="datetime"
+        title="选择开始时间"
+        :loading="isLoadingShow"
+        :min-date="minDate"
+        :max-date="maxDate"
+        :formatter="formatter"
+        @cancel="show = false"
+        @confirm="confirmPicker"
+      />
+    </van-popup>
   </div>
 </template>
 
@@ -41,18 +28,55 @@ export default {
   data () {
     return {
       msg: '商家页面',
+      timeValue: '请选择时间',
       show: false,
-      value: '',
-      showKeyboard: false,
-      passwordVal: ''
+      isLoadingShow: true,
+      currentDate: new Date(),
+      minDate: new Date(),
+      maxDate: new Date(2020, 12, 31),
+      className: ''
     }
   },
   methods: {
-    onInput (value) {
-      this.passwordVal = (this.passwordVal + value).slice(0, 6)
+    // 显示弹窗
+    showPopup () {
+      this.show = true
+      this.isLoadingShow = true
+      setTimeout(() => {
+        this.isLoadingShow = false
+      }, 500)
     },
-    onDelete () {
-      this.passwordVal = this.passwordVal.slice(0, this.passwordVal.length - 1)
+    // 确认选择的时间
+    confirmPicker (val) {
+      let year = val.getFullYear()
+      let month = val.getMonth() + 1
+      let day = val.getDate()
+      let hour = val.getHours()
+      let minute = val.getMinutes()
+      if (month >= 1 && month <= 9) { month = `0${month}` }
+      if (day >= 1 && day <= 9) { day = `0${day}` }
+      if (hour >= 0 && hour <= 9) { hour = `0${hour}` }
+      if (minute >= 0 && minute <= 9) { minute = `0${minute}` }
+      this.className = 'timeClass'
+      this.timeValue = `${year}-${month}-${day} ${hour}:${minute}`
+      this.show = false
+    },
+    // 选项格式化函数
+    formatter (type, value) {
+      if (type === 'year') {
+        return `${value}年`
+      } else if (type === 'month') {
+        return `${value}月`
+      } else if (type === 'day') {
+        return `${value}日`
+      } else if (type === 'hour') {
+        return `${value}时`
+      } else if (type === 'minute') {
+        return `${value}分`
+      } else if (type === 'second') {
+        return `${value}秒`
+      }
+      return value
     }
   }
 }
@@ -60,6 +84,7 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 .seller
-  // margin-top: 20px
-  // text-align: center
+  .timeClass {
+    color: #333;
+  }
 </style>
