9de6edf3f7130a24512ede4ff078c48cd64f2238
ykCoder <megatron0817@foxmail.com>
2020-05-26 15:45:34 +0800

    修改打包路径

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../sellList/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '../sellList/',
 
     /**
      * Source Maps
